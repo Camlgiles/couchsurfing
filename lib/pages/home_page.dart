12e480d8aa817774606456ed2365f80f4699eff5
camlgiles <cameron@lunarcrush.com>
2024-02-08 12:30:41 -0700

    feat: bottom nav added to homepage. FeedPage and ProfilePage added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,24 @@
+import 'package:couchsurfing_flutter_app/pages/feed_page.dart';
+import 'package:couchsurfing_flutter_app/pages/profile_page.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List _pages = [const FeedPage(), const ProfilePage()];
+
+  int _selectedIndex = 0;
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +27,15 @@ class HomePage extends StatelessWidget {
         title: const Text('Couchsurfing'),
         backgroundColor: Colors.blue[600],
       ),
-      body: Scaffold(),
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.blue,
+          currentIndex: _selectedIndex,
+          onTap: _navigateBottomBar,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ]),
+      body: _pages[_selectedIndex],
     );
   }
 }
